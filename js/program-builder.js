0efa9840507232dcf61fee65ab79fc1e29bc6de6
Brandon Thompson <brandotron@gmail.com>
2017-07-31 07:26:13 -0500

    exercises can be removed, updated

--- js/program-builder.js
+++ js/program-builder.js
@@ -55,6 +55,7 @@ Vue.component('loaded-program', {
       '<program-block ' +
         'v-for="item in program.blocks"' + 
         'v-bind:block="item"' +
+        'v-bind:key="item.id"' +
       '></program-block>' +
     '</div>'
 });
@@ -65,6 +66,7 @@ Vue.component('program-block', {
       '<program-week ' +
         'v-for="item in block.weeks"' + 
         'v-bind:week="item"' +
+        'v-bind:key="item.id"' +
       '></program-week>' +
     '</div>'
 });
@@ -75,6 +77,7 @@ Vue.component('program-week', {
       '<program-day ' +
         'v-for="item in week.days"' +
         'v-bind:day="item"' +
+        'v-bind:key="item.id"' +
       '></program-day>' +
     '</div>'
 });
@@ -90,12 +93,14 @@ Vue.component('program-day', {
             '<th class="day-column-head">Reps</th>' +
             '<th class="day-column-head">Weight</th>' +
             '<th class="day-column-head">Note</th>' +
+            '<th class="day-column-head"></th>' +
           '</tr>' +
         '</thead>' +
         '<tbody class="day-body">' +
           '<program-exercise-row ' +
             'v-for="item in day.exercises"' +
             'v-bind:exercise="item"' +
+            'v-bind:key="item.id"' +
           '></program-exercise-row>' +
         '</tbody>' +
       '</table>' + 
@@ -107,6 +112,7 @@ Vue.component('program-day', {
       programBuilder.newExercise.week = this.$parent.week.id;
       programBuilder.newExercise.block = this.$parent.$parent.block.id;
       programBuilder.newExercise.id = this.day.exercises.length;
+      programBuilder.newExercise.mode = 'add';
       programBuilder.newExercise.active = true;
     }
   }
@@ -120,7 +126,46 @@ Vue.component('program-exercise-row', {
       '<td class="day-exercise-cell">{{ exercise.reps }}</td>' +
       '<td class="day-exercise-cell">{{ exercise.weight }}</td>' +
       '<td class="day-exercise-cell">{{ exercise.note }}</td>' +
-    '</tr>'
+      '<td class="day-exercise-cell">' +
+        '<button v-on:click="openEditExercise">Edit</button>' +
+        '<button v-on:click="removeExercise">X</button>' +
+      '</td>' +
+    '</tr>',
+  // data: {
+  //   day: function () {
+  //     return this.$parent.day.id;
+  //   },
+  //   week: function () {
+  //     return this.$parent.$parent.week.id;
+  //   },
+  //   block: function () {
+  //     return this.$parent.$parent.$parent.block.id;
+  //   }
+  // },
+  methods: {
+    removeExercise: function () {
+      var currentBlock = programBuilder.loadedProgram.blocks[this.$parent.$parent.$parent.block.id];
+      var currentWeek = currentBlock.weeks[this.$parent.$parent.week.id];
+      var currentDay = currentWeek.days[this.$parent.day.id];
+
+      currentDay.exercises.splice(this.exercise.id, 1);
+    },
+    openEditExercise: function () {
+      programBuilder.newExercise.day = this.$parent.day.id;
+      programBuilder.newExercise.week = this.$parent.$parent.week.id;
+      programBuilder.newExercise.block = this.$parent.$parent.$parent.block.id;
+      programBuilder.newExercise.id = this.exercise.id;
+
+      programBuilder.newExercise.name = this.exercise.name;
+      programBuilder.newExercise.sets = this.exercise.sets;
+      programBuilder.newExercise.reps = this.exercise.reps;
+      programBuilder.newExercise.weight = this.exercise.weight;
+      programBuilder.newExercise.note = this.exercise.note;
+
+      programBuilder.newExercise.mode = 'update';
+      programBuilder.newExercise.active = true;
+    }
+  }
 });                
 var programBuilder = new Vue({
   el: '#program-builder',
@@ -131,6 +176,7 @@ var programBuilder = new Vue({
     allPrograms: {},
     newExercise: {
       active: false,
+      mode: 'add',
       block: '',
       week: '',
       day: '',
@@ -157,13 +203,29 @@ var programBuilder = new Vue({
       var currentDay = currentWeek.days[this.newExercise.day];
 
       currentDay.exercises.push(exerciseToPush);
-      this.resetInputs();
+      this.resetExerciseInputs();
     },
-    closePanel: function () {
+    updateExercise: function () { //TODO: combine this and above function
+      var exerciseToPush = {
+        id: this.newExercise.id,
+        name: this.newExercise.name,
+        sets: this.newExercise.sets,
+        reps: this.newExercise.reps,
+        weight: this.newExercise.weight,
+        note: this.newExercise.note
+      }
+      var currentBlock = programBuilder.loadedProgram.blocks[this.newExercise.block];
+      var currentWeek = currentBlock.weeks[this.newExercise.week];
+      var currentDay = currentWeek.days[this.newExercise.day];
+
+      currentDay.exercises.splice(this.newExercise.id, 1, exerciseToPush);
+      this.resetExerciseInputs();
+    },
+    closeExercisePanel: function () {
       this.newExercise.active = false;
-      this.resetInputs();
+      this.resetExerciseInputs();
     },
-    resetInputs: function () {
+    resetExerciseInputs: function () {
       //this.active = this.active;
       this.newExercise.block = '';
       this.newExercise.week = '';
@@ -173,6 +235,7 @@ var programBuilder = new Vue({
       this.newExercise.reps = '';
       this.newExercise.weight = '';
       this.newExercise.note = '';
+      this.newExercise.mode = 'add';
     }
   }
 });
